f3c32787a2621cb9fda16f16fc8aabb422c6b8c3
shf-ns <3104467468@qq.com>
2026-06-23 15:27:22 +0800

    feat(style): 调整响应式断点和组件样式
    - 将多个组件的媒体查询断点从 480px 和 768px 统一调整为 786px
    - 调整 Profile 组件高度从 350px 到 320px
    - 调整 Sidebar 在中等屏幕尺寸下的宽度为 15%
    - 在 Profile 组件中添加用户名显示

--- src/component/Profile.tsx
+++ src/component/Profile.tsx
@@ -9,6 +9,7 @@ function Profile(): ReactNode {
         <div className={styles.avatar}>
           <Avatar />
         </div>
+        <p>shf-ns</p>
         <h3>欢迎来到我的个人网站！</h3>
       </div>
       <p className={styles.quote}>quto</p>
